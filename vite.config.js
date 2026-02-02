3bb95a0d36381c0191f38ef48f7e5eb851e8b6df
Daniel Martins <m.dan3ella@gmail.com>
2026-02-02 13:37:13 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Ensure this matches your repo name
+  // Matches your repo name exactly
   base: '/simmorph-studio-alpha/', 
   build: {
     target: 'esnext', 
